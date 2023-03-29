8157f5f9d6dc5db1140f4dd6db21868c4af66c31
lisa3711 <lisa.haitz@uc.edu>
2023-03-29 15:00:22 -0400

    created department references

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -65,6 +65,6 @@ class EmployeesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def employee_params
-      params.require(:employee).permit(:lastname, :firstname, :department, :pronouns, :email, :phone)
+      params.require(:employee).permit(:lastname, :firstname, :department_id, :pronouns, :email, :phone, :title)
     end
 end
--- lib/import_departments_csv.rb
+++ lib/import_departments_csv.rb
@@ -3,7 +3,7 @@ require 'csv'
 # Open the CSV file and iterate over each row
 CSV.foreach('department-data.csv', headers: true) do |row|
   # Create a new instance of the model and set its attributes based on the CSV row
-  department = Department.new(name: row['neame'])
+  department = Department.new(name: row['name'])
   
   # Save the record to the database
   department.save
