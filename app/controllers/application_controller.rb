793b2478c705cd61e322ddc8a8dad5b21dae7eb3
lisa3711 <lisa.haitz@uc.edu>
2023-05-02 12:47:05 -0400

    modify depts, controllers, views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,33 @@
 class ApplicationController < ActionController::Base
-    before_action :config_devise_params, if: :devise_controller?
-    ...  
-    protected
-      def config_devise_params
-          devise_parameter_sanitizer.permit(:sign_in, keys: [ 
-              :first_name,
-              :last_name,
-              :username,
-              :email,
-              :password,
-              :password_confirmation
-              ])
-      end
-      def index
+  rescue_from ActiveRecord::InvalidForeignKey, with: :handle_invalid_foreign_key
+
+  protected
+
+  def handle_invalid_foreign_key
+    flash[:error] = "Cannot delete department with associated employees."
+    redirect_to departments_path
+  end
+
+  def config_devise_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [
+      :first_name,
+      :last_name,
+      :username,
+      :email,
+      :password,
+      :password_confirmation
+    ])
+  end
+
+  def index
     @employees = Employee.all
-     #added below for devise
-      layout :layout_by_resource
-              
-        private
-         def layout_by_resource
-             devise_controller? ? "session" : "application"
-         end     
-              
+    #added below for devise
+    layout :layout_by_resource
+  end
+
+  private
+
+  def layout_by_resource
+    devise_controller? ? "session" : "application"
   end
 end
