eeffe50384cbb6125e7ac119b9b8464e9a2e2e1b
lisa3711 <lisa.haitz@uc.edu>
2023-10-11 17:53:17 -0400

    Tests all passed - coverage is 76.56%

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# spec/factories/users.rb
+
+FactoryBot.define do
+  factory :user do
+    # Define the attributes for the user factory
+    # For example:
+    email { 'user@example.com' }
+    password { 'password' }
+  end
+end
