f856d0ed1ce7de924fa8ec15b8c0fefff5a3b5e7
lisa3711 <lisa.haitz@uc.edu>
2024-02-13 16:23:49 -0500

    removed mailer files

--- app/mailers/login_mailer.rb
+++
@@ -1,7 +0,0 @@
-class LoginMailer < ApplicationMailer
-  def welcome_email(user)
-    @user = user
-    @url = "https://example.com/login"
-    mail(to: user.email, from: "Login Code <lisa.haitz@uc.edu>", subject: "Your Login Code")
-  end
-end
