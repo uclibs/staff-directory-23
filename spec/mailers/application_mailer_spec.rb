450068c99783dcc6126d26ad165fdd41e5c436c5
lisa3711 <lisa.haitz@uc.edu>
2024-03-07 12:30:31 -0500

    updates to mailer and view, add spec tests for mailer

--- spec/mailers/application_mailer_spec.rb
+++ spec/mailers/application_mailer_spec.rb
@@ -1,21 +1,6 @@
-# frozen_string_literal: true
-
+# spec/mailers/application_mailer_spec.rb
 require 'rails_helper'
-
 RSpec.describe ApplicationMailer, type: :mailer do
-  describe 'password_change' do
-    let(:user) { create(:user) }
-
-    it 'renders the subject' do
-      mail = described_class.password_change(user)
-      expect(mail.subject).to eq('Your password has been changed')
-    end
-
-    it 'renders the body' do
-      mail = described_class.password_change(user)
-      expect(mail.body.encoded).to include('Your password has been successfully changed.')
-    end
-  end
-
-  # More test cases for other mailer methods/views
+  # Currently, no specific tests are needed for ApplicationMailer
 end
+
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
+# spec/mailers/previews/user_mailer_preview.rb
 
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_change
+  def password_change
+    # Fetch a user or create a mock user object
+    user = User.first || User.new(email: 'user@example.com', name: 'Sample User')
+    # Call the mailer method and pass the user object
+    UserMailer.password_change(user)
+  end
 end
