e4039842ab087ac34d06def484897060b139bd55
lisa3711 <lisa.haitz@uc.edu>
2024-03-08 16:34:16 -0500

    update routes and controller, spec test for password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get 'user/create'
-  # get 'user/update'
-  # get 'user/delete'
-  # devise_for :views
   devise_for :users, controllers: { passwords: 'passwords' }, skip: [:registrations]
-
+  # Custom routes for password reset
   get 'forgot_password', to: 'passwords#new'
   post 'forgot_password', to: 'passwords#create'
   get 'reset_password', to: 'passwords#edit'
-  # get 'departments/index'
-  # get 'departments/show'
-  # get 'departments/edit'
-  # get 'departments/update'# frozen_string_literal: true
-  #
-  # Rails.application.routes.draw do
-  #   # get 'user/create'
-  #   # get 'user/update'
-  #   # get 'user/delete'
-  #   # devise_for :views
-  #   devise_for :users, controllers: { passwords: 'passwords' }, skip: [:registrations]
-  #
-  #   get 'forgot_password', to: 'passwords#new'
-  #   post 'forgot_password', to: 'passwords#create'
-  #   get 'reset_password', to: 'passwords#edit'
-  #   # get 'departments/index'
-  #   # get 'departments/show'
-  #   # get 'departments/edit'
-  #   # get 'departments/update'
-  #   # get 'departments/new'
-  #   # root 'employees#index' # make the employees#index the root of the application
-  #   root 'employees#index'
-  #   # suggestion: get 'public/index'
-  #   resources :employees
-  #   resources :departments
-  #   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #   # get 'login', to: 'sessions#new', as: :login
-  #   # post 'login', to: 'sessions#create'
-  #   # delete 'logout', to: 'sessions#destroy', as: :logout
-  # end
-  # get 'departments/new'
+
   # root 'employees#index' # make the employees#index the root of the application
   root 'employees#index'
   # suggestion: get 'public/index'
   resources :employees
   resources :departments
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get 'login', to: 'sessions#new', as: :login
-  # post 'login', to: 'sessions#create'
-  # delete 'logout', to: 'sessions#destroy', as: :logout
 end
--- spec/requests/passwords_controller_spec.rb
+++ spec/requests/passwords_controller_spec.rb
@@ -2,26 +2,32 @@
 
 require 'rails_helper'
 
-RSpec.describe PasswordsController, type: :request do
-  describe 'POST /users/password' do
-    let(:user) { create(:user) }
+RSpec.describe PasswordsController, type: :controller do
+  include Devise::Test::ControllerHelpers # Include this if you haven't globally
+
+  before do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+  end
+
+  describe 'POST #create' do
+    let(:user) { FactoryBot.create(:user) } # Assuming you have a factory for users
 
     context 'with valid email' do
-      it 'sends reset password instructions' do
-        post user_password_path, params: { user: { email: user.email } }
-        expect(response).to have_http_status(:ok)
-        # Expectation to check if the response renders the correct view or contains the correct message
+      it 'sends reset password instructions and redirects to root with a success message' do
+        post :create, params: { user: { email: user.email } }
+
+        expect(response).to redirect_to(root_path)
+        expect(flash[:notice]).to eq('Reset password instructions have been sent to your email! Very exciting!')
       end
     end
 
     context 'with invalid email' do
-      it 'returns an error' do
-        post user_password_path, params: { user: { email: 'nonexistent@example.com' } }
+      it 'renders the new template with an error message' do
+        post :create, params: { user: { email: 'nonexistent@example.com' } }
+
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(JSON.parse(response.body)['error']).to eq('Invalid email')
+        expect(response.body).to include('Invalid email')
       end
     end
   end
-
-  # Further tests for `edit` and `update` actions here...
 end
