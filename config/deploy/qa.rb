49aa36edd5e72b3354070e95fe179d51e160e871
lisa3711 <lisa.haitz@uc.edu>
2024-03-08 14:38:16 -0500

    test new mailer updates

---
+++ config/deploy/qa.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+set :rails_env, :production
+set :bundle_without, %w[development test].join(' ')
+set :branch, 'mailerupdates'
+set :default_env, path: '$PATH:/usr/local/bin'
+set :bundle_path, -> { shared_path.join('vendor/bundle') }
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+append :linked_dirs, 'tmp', 'log'
+ask(:username, nil)
+ask(:password, nil, echo: false)
+server 'libappstest.libraries.uc.edu', user: fetch(:username), password: fetch(:password), port: 22, roles: %i[web app db]
+set :deploy_to, '/opt/webapps/staff-directory'
+after 'deploy:updating', 'ruby_update_check'
+after 'deploy:updating', 'init_qp'
+before 'deploy:cleanup', 'start_qp'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,6 +77,11 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.require_master_key = false
 
-  config.action_mailer.default_url_options = { host: 'libappstest.libraries.uc.edu', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'libappstest.libraries.uc.edu' }
   config.mailer_sender = 'lisa.haitz@uc.edu'
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    # SMTP settings here
+  }
 end
