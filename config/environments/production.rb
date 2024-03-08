918c17dd23cde9bf26665b5d14ecdaa4f3acedc6
lisa3711 <lisa.haitz@uc.edu>
2024-03-08 15:31:04 -0500

    trying different settings for host mismatch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
   # Set the host for Devise mailer URLs
-  # config.action_mailer.default_url_options = { host: 'libappstest.libraries.uc.edu' }
+  config.action_mailer.default_url_options = { host: 'libappstest.libraries.uc.edu' }
   config.mailer_sender = 'lisa.haitz@uc.edu'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
