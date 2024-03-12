24c68ed0e2435b8da40aecc188c63360d69587e6
lisa3711 <lisa.haitz@uc.edu>
2024-03-12 16:19:55 -0400

    correct seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,21 +18,33 @@
 Department.delete_all
 Employee.delete_all
 
-# Create departments
-departments = ['CEAS', 'Administration', 'CCM', 'Application Development', 'Library IT', 'Classics', 'HR',
-               'Library Communications']
-departments.each do |name|
-  Department.create(name:)
-end
-
-# Create employees
-employees = [
-  # format  { lastname: 'Haitz', firstname: 'Lisa', email: 'lisa.haitz@uc.edu', phone: '556-1431', title: 'Software Developer 2',
-  # format department: Department.find_by(name: 'Application Development') },
-  # format  }
-  #
+departments = [
+  { name: 'CEAS' },
+  { name: 'Administration' },
+  { name: 'CCM' },
+  { name: 'Application Development' },
+  { name: 'Library IT' },
+  { name: 'Classics' },
+  { name: 'HR' },
+  { name: 'Library Communications' }
 ]
 
-employees.each do |employee|
-  Employee.create(employee)
-end
+Department.create(departments)
+
+# Add Employees
+employees = [
+  { lastname: 'Haitz', firstname: 'Lisa', email: 'lisa.haitz@uc.edu', phone: '556-1431', title: 'Software Developer 2',
+    department_id: Department.find_by(name: 'Application Development').id },
+  { lastname: 'Hart', firstname: 'Olga', email: 'olga.hart@uc.edu', phone: '556-1850',
+    title: 'Associate Senior Librarian', department_id: Department.find_by(name: 'Administration').id },
+  { lastname: 'Norris', firstname: 'Melissa', email: 'melissa.norris@uc.edu', phone: '556-1558',
+    title: 'Directory of Library Communication', department_id: Department.find_by(name: 'Library Communications').id },
+  { lastname: 'Baldwin', firstname: 'Ted', email: 'michael.braunlin@uc.edu', phone: '556-4211',
+    title: 'Associate Senior Librarian', department_id: Department.find_by(name: 'CEAS').id },
+  { lastname: 'Braunlin', firstname: 'Michael', email: 'lisa.haitz@uc.edu', phone: '556-1333',
+    title: 'Assistant Head Circulation', department_id: Department.find_by(name: 'Classics').id },
+  { lastname: 'Chang', firstname: 'May', email: 'may.chang@uc.edu', phone: '556-3165',
+    title: 'Chief Technology Officer', department_id: Department.find_by(name: 'Library IT').id }
+]
+
+Employee.create(employees)
