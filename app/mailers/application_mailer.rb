339596d4d278f10637c73794b81d044b586a9247
lisa3711 <lisa.haitz@uc.edu>
2024-04-02 13:02:50 -0400

    fix rubocop error

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: default_sender = ENV['STADIR_MAILER_FROM'] || 'uclappdev@uc.edu'
+  default from: ENV['STADIR_MAILER_FROM'] || 'uclappdev@uc.edu'
   layout 'mailer'
 end
