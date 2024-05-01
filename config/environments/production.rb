736b1cc6785631730435f3a5defcd7ae8a859e3e
lisa3711 <lisa.haitz@uc.edu>
2024-05-01 16:54:20 -0400

    update config/prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,7 +106,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     # 'address' specifies the address of the server that will handle email sending.
-    address: ENV['MAIL_SMTP_ADDRESS'],
+    address: 'libappstest.libraries.uc.edu',
     # 'port' specifies which port to use on the SMTP server.
     # Port 25 is the default port for SMTP servers like Postfix.
     enable_starttls_auto: true,
