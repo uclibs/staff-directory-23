41081603ec7646f52a1b71fa80838a9e77b1d9e6
lisa3711 <lisa.haitz@uc.edu>
2024-05-13 11:52:57 -0400

    adjust settings for cache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,8 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  #config.assets.debug = false
+  # for debugging javascript on dev.
   config.assets.compile = true
   config.assets.digest = false
 
