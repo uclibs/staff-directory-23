91e32001efcd4145dbac4c2b2c48f9ec942e0251
Lisa Haitz <lisa.haitz@uc.edu>
2024-05-16 11:47:00 -0400

    Merge QA into Main for production deploy (#184)
    * update start script (#168)
    
    * Fix productionfile (#177)
    
    * change env value
    
    * spelling error
    
    * css changes (#173)
    
    * css updates
    
    * update css
    
    * change signin value
    
    * change h1 to h2
    
    * fix cache on development.rb
    
    * adjust settings for cache
    
    * fix rubocop spacing error
    
    * corrects public server enabled line (#180)
    
    * Bump nokogiri from 1.16.4 to 1.16.5 (#179)
    
    Bumps [nokogiri](https://github.com/sparklemotion/nokogiri) from 1.16.4 to 1.16.5.
    - [Release notes](https://github.com/sparklemotion/nokogiri/releases)
    - [Changelog](https://github.com/sparklemotion/nokogiri/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/sparklemotion/nokogiri/compare/v1.16.4...v1.16.5)
    
    ---
    updated-dependencies:
    - dependency-name: nokogiri
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * modified to fix #178 (#183)
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,10 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  # config.assets.debug = false
+  # for debugging javascript on dev.
+  config.assets.compile = true
+  config.assets.digest = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -101,7 +101,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Set the host for Devise mailer URLs
-  config.action_mailer.default_url_options = { host: 'libappstest.libraries.uc.edu' }
+  config.action_mailer.default_url_options = { host: ENV['STADIR_PRODUCTION_MAILER_URL'] }
   config.mailer_from = 'uclappdev@uc.edu'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
