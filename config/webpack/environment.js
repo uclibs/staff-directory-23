5d18c981eb1eecb95ec285ebe74409a40aac166a
Lisa Haitz <lisa.haitz@uc.edu>
2025-03-04 11:43:10 -0500

    updated webpack (#232)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+process.env.NODE_OPTIONS = '--openssl-legacy-provider';
+// Fully disable node polyfills to fix Webpack 5 issues
+environment.config.merge({
+    resolve: {
+        fallback: {
+            fs: false,
+            net: false,
+            tls: false,
+            dgram: false,
+            child_process: false
+        }
+    },
+    node: false
+});
+
+module.exports = environment;
