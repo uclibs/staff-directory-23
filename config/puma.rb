968b818817c54e228e3436dc29741230f4097b09
Lisa Haitz <lisa.haitz@uc.edu>
2025-11-14 15:30:54 -0500

    Try deploy (#279)
    * try to get rbenv to use ruby 3.3.9
    
    * update package.json file to build shakapacker
    
    * push updated puma.rb
    
    * updated corrupted Puma file
    
    * fix rubocop error on puma.rb file

--- config/puma.rb
+++ config/puma.rb
@@ -1,52 +1,11 @@
 # frozen_string_literal: true
 
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
-threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+threads_count = ENV.fetch('RAILS_MAX_THREADS', 3)
 threads threads_count, threads_count
 
-# Specifies the `environment` that Puma will run in.
-#
-# environment ENV.fetch('RAILS_ENV', 'development')
+# Use Unix socket instead of TCP port
+bind 'unix:///opt/webapps/staff-directory/current/tmp/puma/puma.sock'
 
-app_dir = File.expand_path('..', __dir__)
-
-if ENV.fetch('RAILS_ENV') == 'production'
-  # Set the working directory
-  directory app_dir.to_s
-  # Set up socket location
-  bind "unix://#{app_dir}/tmp/puma/puma.sock"
-  # Log to files
-  stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
-else
-  port ENV.fetch('PORT', 3000)
-end
-
-# Set master PID and state locations
-pidfile "#{app_dir}/tmp/puma/pid"
-state_path "#{app_dir}/tmp/puma/state"
-
-# Puma control rack application
-activate_control_app
-
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
-
-# Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+pidfile ENV['PIDFILE'] if ENV['PIDFILE']
