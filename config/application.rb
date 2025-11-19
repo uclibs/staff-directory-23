7dd8489e53158e54a1a5d97964fd038d17c7a100
Lisa Haitz <lisa.haitz@uc.edu>
2025-11-19 15:45:46 -0500

    Try deploy (#282)
    * try to get rbenv to use ruby 3.3.9
    
    * update package.json file to build shakapacker
    
    * push updated puma.rb
    
    * updated corrupted Puma file
    
    * fix rubocop error on puma.rb file
    
    * Update to Rails 7.2 defaults and fix test suite
    
    * per reviewer:We still need Puma’s production logs and PID tracking.
    
    * Address code review feedback
    
    - Restore production mailer configuration for Devise password resets
    - Clean up puma.rb (remove duplicate bind statements)
    - Fix Rails.logger.debug to puts in Capistrano deploy script
    
    * more corerctions for comment per reviewer

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ Bundler.require(*Rails.groups)
 module Staffdirectory
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.2
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # Please, add to the `ignore` list any other `git checkout try-deploylib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -13,7 +13,28 @@ ask(:password, nil, echo: false)
 server 'libapps.libraries.uc.edu', user: fetch(:username), password: fetch(:password), port: 22, roles: %i[web app db]
 ask(:value, 'Have you submitted and received an approved Change Management Request? (Y or Yes)')
 unless fetch(:value).match?(/\A(?i:yes|y)\z/)
-  Rails.logger.debug "\nNo confirmation - deploy cancelled!"
+  # frozen_string_literal: true
+
+  set :rails_env, :production
+  set :bundle_without, %w[development test].join(' ')
+  set :branch, 'main'
+  set :default_env, path: '$PATH:/usr/local/bin'
+  set :bundle_path, -> { shared_path.join('vendor/bundle') }
+  set :rbenv_prefix,
+      "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+  append :linked_dirs, 'tmp', 'log'
+  ask(:username, nil)
+  ask(:password, nil, echo: false)
+  server 'libapps.libraries.uc.edu', user: fetch(:username), password: fetch(:password), port: 22, roles: %i[web app db]
+  ask(:value, 'Have you submitted and received an approved Change Management Request? (Y or Yes)')
+  unless fetch(:value).match?(/\A(?i:yes|y)\z/)
+    puts "\nNo confirmation - deploy cancelled!"
+    exit
+  end
+  set :deploy_to, '/opt/webapps/staff-directory'
+  after 'deploy:updating', 'ruby_update_check'
+  after 'deploy:updating', 'init_qp'
+  before 'deploy:cleanup', 'start_qp'
   exit
 end
 set :deploy_to, '/opt/webapps/staff-directory'
