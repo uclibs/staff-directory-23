91b0612b9daac4e1329c7ecc22224a0808d059fe
lisa3711 <lisa.haitz@uc.edu>
2025-11-19 15:02:30 -0500

    per reviewer:We still need Puma’s production logs and PID tracking.

--- config/puma.rb
+++ config/puma.rb
@@ -3,9 +3,18 @@
 threads_count = ENV.fetch('RAILS_MAX_THREADS', 3)
 threads threads_count, threads_count
 
-# Use Unix socket instead of TCP port
-bind 'unix:///opt/webapps/staff-directory/current/tmp/puma/puma.sock'
-
 plugin :tmp_restart
 
-pidfile ENV['PIDFILE'] if ENV['PIDFILE']
+app_dir = File.expand_path('..', __dir__)
+
+if ENV.fetch('RAILS_ENV', 'development') == 'production'
+  directory app_dir.to_s
+  bind "unix://#{app_dir}/tmp/puma/puma.sock"
+  stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+else
+  port ENV.fetch('PORT', 3000)
+end
+
+pidfile "#{app_dir}/tmp/puma/pid"
+state_path "#{app_dir}/tmp/puma/state"
+activate_control_app
