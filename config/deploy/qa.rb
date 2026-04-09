2532144782a9416f0c5daa55455c01f08691cf22
Janell Huyck <57018862+Janell-Huyck@users.noreply.github.com>
2026-04-09 09:31:50 -0400

    Update comments in gemfile, return qa deploy to qa branch

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -2,7 +2,7 @@
 
 set :rails_env, :production
 set :bundle_without, %w[development test].join(' ')
-set :branch, 'LIBSTAF1-5-update-concurrent-ruby'
+set :branch, 'qa'
 set :default_env, path: '$PATH:/usr/local/bin'
 set :bundle_path, -> { shared_path.join('vendor/bundle') }
 set :rbenv_prefix,
